b1ba60ac84ff22c52d9ee7f18d4f766d48674ce7
Dan Albrecht <Dan-Albrecht@users.noreply.github.com>
2020-02-16 23:05:29 -0800

    Add stubs

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dedupe_files
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
