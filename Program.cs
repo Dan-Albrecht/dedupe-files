affa1714659db30d7279f80fd0d54cf7ec4e45f4
Dan Albrecht <Dan-Albrecht@users.noreply.github.com>
2020-02-17 16:50:08 -0800

    Linq4Life

--- Program.cs
+++ Program.cs
@@ -1,12 +1,46 @@
-﻿using System;
-
-namespace dedupe_files
+﻿namespace DedupeFiles
 {
-    class Program
+    using System;
+    using System.CommandLine;
+    using System.CommandLine.Invocation;
+    using System.IO;
+    using System.Threading.Tasks;
+
+    internal class Program
     {
-        static void Main(string[] args)
+        private static async Task<int> Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var rootCommand = new RootCommand("Finds and deals with duplcate files")
+            {
+                new Option(
+                    new[]{ "-p", "--path" },
+                    "Path to recurrsively sacn")
+                {
+                    Argument = new Argument<string>(getDefaultValue: () => Environment.CurrentDirectory)
+                }
+            };
+
+            rootCommand.Handler = CommandHandler.Create<DirectoryInfo>((directoryInfo) =>
+            {
+                try
+                {
+                    Searcher searcher = new Searcher(directoryInfo);
+                    searcher.Search();
+
+                    return 0;
+                }
+                catch (Exception e)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(e);
+                    Console.ResetColor();
+
+                    return -1;
+                }
+            });
+
+            // Parse the incoming args and invoke the handler
+            return await rootCommand.InvokeAsync(args);
         }
     }
 }
---
+++ Searcher.cs
@@ -0,0 +1,127 @@
+﻿namespace DedupeFiles
+{
+    using System;
+    using System.Collections.Concurrent;
+    using System.Collections.Generic;
+    using System.Diagnostics.CodeAnalysis;
+    using System.IO;
+    using System.Linq;
+    using System.Security.Cryptography;
+    using System.Threading.Tasks;
+
+    public class Searcher
+    {
+        private readonly DirectoryInfo searchPath;
+
+        public Searcher(DirectoryInfo searchPath)
+        {
+            this.searchPath = searchPath ?? throw new ArgumentNullException(nameof(searchPath));
+        }
+
+        private class ByteComp : IEqualityComparer<byte[]>
+        {
+            public static ByteComp Instance = new ByteComp();
+
+            public bool Equals([AllowNull] byte[] x, [AllowNull] byte[] y)
+            {
+                if (x is null)
+                {
+                    if (y is null)
+                    {
+                        return true;
+                    }
+
+                    return false;
+                }
+
+                if (y is null)
+                {
+                    return false;
+                }
+
+                if (x.LongLength != y.LongLength)
+                {
+                    return false;
+                }
+
+                for (long i = 0; i < x.LongLength; i++)
+                {
+                    if (x[i] != y[i])
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+
+            public int GetHashCode([DisallowNull] byte[] obj)
+            {
+                // lol, whatever
+                return obj.Length;
+            }
+        }
+
+        internal void Search()
+        {
+            searchPath
+                .EnumerateFiles("*", SearchOption.AllDirectories)
+                .ToLookup(item => item.Length) // Files of different lengths aren't the same
+                .Where(item => item.Count() > 1)
+                .Select(item => item
+                    .ToLookup(item2 => GetTheMiddleBytes(item2), ByteComp.Instance)
+                    .Where(item2 => item2.Count() > 1)
+                    .Select(item2 => item2
+                        .ToLookup(item3 => GetMD5(item3), ByteComp.Instance)
+                        .Where(item3 => item3.Count() > 1)
+                        .Select(item3 => string.Join("; ", item3.Select(item4 => item4.FullName)) + Environment.NewLine)))
+                .AsParallel()
+                .SelectMany(item => item)
+                .SelectMany(item => item)
+                .ForAll(item => Console.WriteLine(item));
+        }
+
+
+        private byte[] GetMD5(FileInfo item)
+        {
+            using (var md5 = MD5.Create())
+            using (var file = File.OpenRead(item.FullName))
+            {
+                byte[] result = md5.ComputeHash(file);
+                return result;
+            }
+        }
+
+        private byte[] GetTheMiddleBytes(FileInfo item)
+        {
+            const int length = 1024;
+            if (item.Length <= length)
+            {
+                return File.ReadAllBytes(item.FullName);
+            }
+
+            // Quick way to get bytes from around the middle of the file
+            // Assumption is begining and ending might be fixed data (e.g. headers) and won't be sufficently unique
+            int middleStart = (int)(item.Length / 2);
+            middleStart -= length;
+
+            if (middleStart < 0)
+            {
+                middleStart = 0;
+            }
+
+            using (var file = File.OpenRead(item.FullName))
+            {
+                file.Seek(middleStart, SeekOrigin.Begin);
+                byte[] theBytes = new byte[length];
+                int read = file.Read(theBytes, 0, length);
+                if (read != length)
+                {
+                    throw new InvalidOperationException($"Something screwed up {read} != {length}");
+                }
+
+                return theBytes;
+            }
+        }
+    }
+}
