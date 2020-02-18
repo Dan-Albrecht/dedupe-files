142f0c5560e661f414d3b45285b040615904db97
Dan Albrecht <Dan-Albrecht@users.noreply.github.com>
2020-02-17 23:12:02 -0800

    add at least

--- Searcher.cs
+++ Searcher.cs
@@ -1,13 +1,38 @@
 ﻿namespace DedupeFiles
 {
     using System;
-    using System.Collections.Concurrent;
     using System.Collections.Generic;
     using System.Diagnostics.CodeAnalysis;
     using System.IO;
     using System.Linq;
     using System.Security.Cryptography;
-    using System.Threading.Tasks;
+
+    internal static class Exts
+    {
+        public static bool AtLeast<T>(this IEnumerable<T> stuff, int atLeast)
+        {
+            if (stuff == null)
+            {
+                return false;
+            }
+
+            if (atLeast == 0)
+            {
+                return true;
+            }
+
+            int count = 0;
+            foreach (T t in stuff)
+            {
+                if (++count >= atLeast)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
 
     public class Searcher
     {
@@ -67,13 +92,13 @@
             searchPath
                 .EnumerateFiles("*", SearchOption.AllDirectories)
                 .ToLookup(item => item.Length) // Files of different lengths aren't the same
-                .Where(item => item.Count() > 1)
+                .Where(item => item.AtLeast(2))
                 .Select(item => item
                     .ToLookup(item2 => GetTheMiddleBytes(item2), ByteComp.Instance)
-                    .Where(item2 => item2.Count() > 1)
+                    .Where(item2 => item2.AtLeast(2))
                     .Select(item2 => item2
                         .ToLookup(item3 => GetMD5(item3), ByteComp.Instance)
-                        .Where(item3 => item3.Count() > 1)
+                        .Where(item3 => item3.AtLeast(2))
                         .Select(item3 => string.Join("; ", item3.Select(item4 => item4.FullName)) + Environment.NewLine)))
                 .AsParallel()
                 .SelectMany(item => item)
@@ -94,33 +119,41 @@
 
         private byte[] GetTheMiddleBytes(FileInfo item)
         {
-            const int length = 1024;
-            if (item.Length <= length)
+            try
             {
-                return File.ReadAllBytes(item.FullName);
-            }
-
-            // Quick way to get bytes from around the middle of the file
-            // Assumption is begining and ending might be fixed data (e.g. headers) and won't be sufficently unique
-            int middleStart = (int)(item.Length / 2);
-            middleStart -= length;
-
-            if (middleStart < 0)
-            {
-                middleStart = 0;
-            }
-
-            using (var file = File.OpenRead(item.FullName))
-            {
-                file.Seek(middleStart, SeekOrigin.Begin);
-                byte[] theBytes = new byte[length];
-                int read = file.Read(theBytes, 0, length);
-                if (read != length)
+                const int length = 1024;
+                if (item.Length <= length)
                 {
-                    throw new InvalidOperationException($"Something screwed up {read} != {length}");
+                    return File.ReadAllBytes(item.FullName);
                 }
 
-                return theBytes;
+                // Quick way to get bytes from around the middle of the file
+                // Assumption is begining and ending might be fixed data (e.g. headers) and won't be sufficently unique
+                int middleStart = (int)(item.Length / 2);
+                middleStart -= length;
+
+                if (middleStart < 0)
+                {
+                    middleStart = 0;
+                }
+
+                using (var file = File.OpenRead(item.FullName))
+                {
+                    file.Seek(middleStart, SeekOrigin.Begin);
+                    byte[] theBytes = new byte[length];
+                    int read = file.Read(theBytes, 0, length);
+                    if (read != length)
+                    {
+                        throw new InvalidOperationException($"Something screwed up {read} != {length}");
+                    }
+
+                    return theBytes;
+                }
+            }
+            catch (IOException ioException)
+            {
+                ioException.ToString();
+                return null;
             }
         }
     }
